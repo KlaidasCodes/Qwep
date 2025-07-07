ac36dc61eff63db2ff315a09c1214e87acd6585e
curious_ad <klaidas.mileris@gmail.com>
2025-07-07 11:32:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import hashlib
+import secrets
+
+# TODO-1 function that generates a secure password
+
+# TODO-2: fn that encrypts the password with a salt (we need access to the password, can't hash it)
+
+# TODO-3: 

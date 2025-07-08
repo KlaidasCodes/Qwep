179d8dd700c9cac74ef01f4eeacdcb1852228ea1
curious_ad <klaidas.mileris@gmail.com>
2025-07-08 16:19:48 +0100

    quite a lot of changes, started adding the logic of initializing the jsons

---
+++ interface.py
@@ -0,0 +1,39 @@
+import os
+import json
+from my_functions import *
+import secrets
+
+# replace the wild card later
+
+def main():
+    new_line = "\n\t-"
+    new_or_old = input(f"Do you already have password manager account? (Y/N) {new_line}").lower()
+
+
+    if new_or_old == "y":
+        # doesn't need new acc
+        pass
+    elif new_or_old == "n":
+        # later package up into just one function new_acc() or something
+
+        path_to_json, amount_of_pots = create_pw_manager_json()
+        incorrect_format = True
+        while incorrect_format:
+            real_pw_pot: int = int(input(f"Input a number 1-{amount_of_pots} to specify the real pot{new_line}"))
+            if 0 < real_pw_pot <= amount_of_pots:
+                real_pot_name: str = f"data {real_pw_pot}"
+                incorrect_format = False
+            else:
+                print("Incorrect input, try again.")   
+            # ask the user to provide a secure master-password and then encrypt this specific pot
+        # encrypt the rest of the pots with randomly generated keys, no need to store them 
+        establish_honeypots_and_encrypt(amount_of_pots, path_to_json)
+
+
+
+    else: 
+        print("Your input was not recognized.")
+
+if __name__ == "__main__":
+    main()
+
---
+++ my_functions.py
@@ -0,0 +1,60 @@
+def create_pw_manager_json():
+    """Creates a new json file with the pw manager with a specific number of honeypots.
+    Returns path to json | amount of pots"""
+    import os
+    import json
+    new_line = "\n\t-"
+
+    new_place = "/home/curious_ad/Documents"
+    # UNCOMMENT THIS FOR REAL FUNCTIONALITY
+    # new_place = input(f"Input the absolute directory of where you would like the password data located (or relative, but be mindful of where the script is being run){new_line}")
+    dir_exists = os.path.isdir(new_place)
+    if not dir_exists:
+        print("Could not find this directory. Make sure it exists and try again!")
+    else:
+        honeypot_amount: int = int(input(f"How many honeypots would you like to initialize? (Recommended at least 2) {new_line}"))
+        initial_json = {}
+        for i in range(honeypot_amount + 1):
+            initial_json[f"data {i + 1}"] = {
+                "data": {
+                    "nonce": "",
+                    "ciphertext": "",
+                    "tag": ""
+                },
+                "salt": "",
+                "iv": ""
+            }
+        with open(f"{new_place}/password_manager.json", "w") as f:            
+            # initializes the json file and its format 
+            json.dump(initial_json, f, indent=4)
+        print(f"Congratulations! Your passwords will now be stored at the directory {new_place}/password_manager.json")
+    return f"{new_place}/password_manager.json", honeypot_amount + 1
+        
+
+
+def establish_honeypots_and_encrypt(amount_of_pots: int, path_to_json: str):
+    """Inputs: amount_of_pots: 
+    Amount of data pots in the json file
+    path_to_json: absolute path to the json file 
+    
+    Returns: void
+    
+    Explanation:Fills all pots in json with fake honeypot data 
+    """
+    import json
+    from secrets import token_bytes
+    with open(path_to_json, "r") as f:
+        the_file = json.load(f)
+    for i in range(amount_of_pots):
+        temp_nonce_hex = token_bytes(12).hex()
+        temp_tag_hex = token_bytes(16).hex()
+        temp_data_hex = token_bytes(100).hex()
+        the_file[f"data {i + 1}"]["data"]["nonce"] = temp_nonce_hex
+        the_file[f"data {i + 1}"]["data"]["ciphertext"] = temp_data_hex
+        the_file[f"data {i + 1}"]["data"]["tag"] = temp_tag_hex
+    with open(path_to_json, "w") as file:
+        json.dump(the_file, file, indent=4)
+    print("Placeholder information filled in successfully!\n")
+    print(f"Your password manager will host {amount_of_pots} data pots. Only one of them is going to host your actual passwords - the rest of them will be honeypots" 
+        f" with randomly generated information. \nOnce you pick the 'real' data pot, remember which one it is.")
+
